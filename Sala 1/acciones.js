0d4f069a410857f755985530128c0ed20de7b590
Julian Restrepo <jarajulianrestrepo@hotmail.com>
2023-01-22 11:17:54 -0500

    Sala 1

--- Sala 1/acciones.js
+++ Sala 1/acciones.js
@@ -1,3 +1,5 @@
+const celda = document.querySelector('.celda')
+const saliste = document.querySelector('.saliste')
 const botonSalida = document.querySelector('.salida .boton')
 const tablero = document.querySelector('.tablero')
 botonSalida.addEventListener('click', () => {
@@ -6,7 +8,7 @@ botonSalida.addEventListener('click', () => {
   setTimeout(() => tablero.classList.remove('oculto'), 100)
 })
 
-const botonTablero = document.querySelector('.tablero .button')
+const botonTablero = document.querySelector('.tablero .validar')
 const n1 = document.getElementById('numero1')
 const n2 = document.getElementById('numero2')
 const n3 = document.getElementById('numero3')
@@ -15,7 +17,8 @@ botonTablero.addEventListener('click', () => {
   let intentos = parseInt(window.localStorage.getItem(val), 10) || 0
   if(intentos < 3){
     if(n1.value == 7 && n2.value == 30 && n3.value == 23){
-      alert('Saliste')
+      setTimeout(() => celda.classList.add('oculto'), 100)
+      setTimeout(() => saliste.classList.remove('oculto'), 100)
     }else{
       window.localStorage.setItem(val, intentos + 1)
       alert(`Fallaste, te quedan ${3 - (intentos + 1)} intentos`)
@@ -23,6 +26,11 @@ botonTablero.addEventListener('click', () => {
   }else{
     alert('No puedes volver a intentar')
   }
+})
+
+const botonSalir = document.querySelector('.tablero .salir')
+botonSalir.addEventListener('click', () => {
+  setTimeout(() => tablero.classList.add('oculto'), 100)
 })
 
 const botonSanitario = document.querySelector('.sanitario .boton')
